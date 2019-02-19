84fc887797aaf0567f7e133f933392aa7ab412bd
ztychr <33706390+ztychr@users.noreply.github.com>
2019-02-19 21:57:19 +0100

    Update wiki-map_0.1.5.py
    false safe sat i while loop

--- wiki-map_0.1.5.py
+++ wiki-map_0.1.5.py
@@ -4,40 +4,42 @@ import urllib.parse
 
 def crawl():
 
-    rawinput = input('Enter your searchstring: ')
+    while True:
 
-    searchstring = rawinput.replace(" ", "_")
-    r = requests.get('https://wikipedia.org/wiki/' + searchstring) # hent url med en webhandler
+        rawinput = input('Enter your searchstring: ')
+        searchstring = rawinput.replace(" ", "_")
 
-    bs = BeautifulSoup(r.text, features='lxml') # lav
+        r = requests.get('https://wikipedia.org/wiki/' + searchstring) # hent url med en webhandler
 
-    titles = [] # laver tom liste
-    urls = [] # laver tom liste
+        bs = BeautifulSoup(r.text, features='lxml') # lav
 
-    for i in bs.find_all('p'): # find alle <p> tags på en wikipedia-side
-        for j in i.find_all('a'): # find alle <a> tags i <p> tags
-            href = j.get('href') # find href værdien i alle <a> tags
-            li = href.replace(" ", "_")
+        titles = [] # laver tom liste
+        urls = [] # laver tom liste
 
-            title = j.get('title') # laver en variable der fetcher titlen på linket
-            links = 'https://wikipedia.org' + li # laver en variable der indeholder det fulde link
+        for i in bs.find_all('p'): # find alle <p> tags på en wikipedia-side
+            for j in i.find_all('a'): # find alle <a> tags i <p> tags
+                li = j.get('href') # find href værdien i alle <a> tags
 
-            if j.get('title') != None and j.get('href') != None: # sorterer alle links som ikke har en titel fra
-                urls.append(links)
-                titles.append(title)
+                title = j.get('title') # laver en variable der fetcher titlen på linket
+                links = 'https://wikipedia.org' + li # laver en variable der indeholder det fulde link
 
+                if j.get('title') != None and j.get('href') != None: # sorterer alle links som ikke har en titel fra
+                    urls.append(links)
+                    titles.append(title)
 
-    print("root url: " + r.url)
-    print('URL\'s  :  ' + str((len(urls)))) # print længde af liste
-    print('Titles :  ' + str((len(titles))) + '\n') # print længde af liste
+        print("root url: " + r.url)
+        print('URL\'s  :  ' + str((len(urls)))) # print længde af liste
+        print('Titles :  ' + str((len(titles))) + '\n') # print længde af liste
 
-    # print -->
-    # titles[1], urls[1],
-    # tiles[2], urls[2]
-    # ...
+        if not titles:
+            print('No articles were found.')
+            continue
+        else:
+            break
+
+    # prints title, url...
     for x, y in zip(titles, urls):
         urllib.parse.quote_plus(x, y)
         print(x + ' :', y)
-
 
 crawl()
