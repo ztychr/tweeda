3f4c5f40ecda0c595a48b7fbbccfdcd1baa1148d
S3j5b0 <44634922+S3j5b0@users.noreply.github.com>
2019-03-25 23:35:59 +0100

    Update Post.py

--- Post.py
+++ Post.py
@@ -1,9 +1,12 @@
+#the class of our tweet/post object. Is made so that we have a list of objects that we can manipulate
+# with our sorting algorithms, without getting our lists ruined
+
 class tweet:
 
 
 
     def __init__(self, userName, likes, replys, retweets, message):
-        self.username = userName
+        self.userName = userName
         self.likes = likes
         self.replys = replys
         self.retweets = retweets
@@ -12,8 +15,9 @@ class tweet:
         self.mentions = self.message.count('@')
         self.hastags = self.message.count('#')
 
+
     def get_userName(self):
-        return self.username
+        return self.userName
     def get_likes(self):
         return self.likes
     def get_replys(self):
@@ -25,11 +29,14 @@ class tweet:
     def get_lenmessage(self):
         return self.lenMessage
 
+    #simply a method to print all attribute of an object
+
     def print_all(self):
-        print("username: " + self.username)
+        print("username: " + self.userName)
         print("likes: " + str(self.likes))
         print("replys: " + str(self.replys))
         print("retweets: " + str(self.retweets))
         print("message: "+ self.message)
         print("length of message: " + str(self.lenMessage))
-        return self.username, self.likes,self.replys,self.retweets,self.message,self.lenMessage
+        print("\n\n")
+        return self.userName, self.likes,self.replys,self.retweets,self.message,self.lenMessage
