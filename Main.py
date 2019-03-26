b4860a16fd3523dff93a4d17165e6e3f702df373
S3j5b0 <44634922+S3j5b0@users.noreply.github.com>
2019-03-26 17:50:28 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,40 @@
 import Scraping
 import statistical
 
-newscrape = Scraping.Scraping("realdonaldtrump", 24)
+
+
+newscrape = Scraping.Scraping("realdonaldtrump", 10)
+
+Secondscrape = Scraping.Scraping("realdonaldtrump", 25)
 
 newscrape.scrape_data()
 
-postlist = newscrape.get_posts()
-
-onepost = postlist[0]
-
-likes = onepost.get_likes()
-likesplusone = statistical.statistical.plusone(onepost.get_likes())
+allpost = newscrape.get_posts()
 
 
-#print(get.getObjectlist_len());
+listofposts = newscrape.get_posts()
+sortedList_likes = newscrape.bubble_sort('likes')
+reversesort_replys = newscrape.bubble_sort_reverse('replys')
+
+averagelikes = statistical.statistical.get_average(sortedList_likes, 'likes')
+medianlikes = statistical.statistical.get_median(sortedList_likes, 'likes')
+
+Reply_frequence = statistical.statistical.frequency_grouping(sortedList_likes, 'replys', 1000, 5000, 10000,20000, 50000)
+print("average amount of likes: "+ str(averagelikes))
+print("median likes: " + str(medianlikes))
+
+
+deviation = statistical.statistical.standard_deviation(listofposts, 'likes')
+
+native_deviation = statistical.statistical.native_sd(listofposts, 'likes')
+
+
+
+
+
+
+#reversSortPost = newscrape.bubble_sort_reverse("likes")
+
+#for i in bubblesort:
+ #   print(i.print_all())
 
