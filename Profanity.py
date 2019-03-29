6828a7f707182716820157eff616a6d025370708
Joakim <joakimhey@gmail.com>
2019-03-29 17:16:58 +0100

    .py add lol

---
+++ Profanity.py
@@ -0,0 +1,20 @@
+import profanity
+
+
+class Profanity:
+
+    def search_profanity(self, message):
+
+        for word in message:
+            if word in message:
+                print("Word", word)
+
+    def try_search(self):
+
+        li = ["Wall", "Trump", "Shitheads"]
+
+        if "Wall" in list:
+            print("Wall", list)
+
+        if "Shit" not in li:
+            print("Shit", li)
